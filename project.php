62701684f904cf9bb59777df3c751b1673cc9775
DESKTOP-NC4UFAF\vartie <vartie@gmail.com>
2017-05-14 20:01:03 -0400

    *add login template

--- project.php
+++ project.php
@@ -6,9 +6,9 @@ session_start();
 require_once '/vendor/autoload.php';
 
 DB::$host = '127.0.0.1';
-DB::$user = 'slimtodo';//change
-DB::$password = '0BSHlBr1EzbrXU50';//change
-DB::$dbName = 'slimtodo';//change
+DB::$user = 'test'; //change
+DB::$password = '5Cq5KgBkz9713wz0'; //change
+DB::$dbName = 'test'; //change
 DB::$port = 3333;
 DB::$encoding = 'utf8';
 
@@ -32,9 +32,33 @@ $twig = $app->view()->getEnvironment();
 $twig->addGlobal('user', $_SESSION['user']);
 
 //State 1;First show
-$app->get('/',function() use($app){
-    $app->render('master.html.twig');
+$app->get('/', function() use($app) {
+    $app->render('login.html.twig');
 });
+
+$app->post('/', function() use ($app) {
+    $username = $app->request()->post('username');
+    $pass = $app->request()->post('pass');
+    // verification    
+    $error = false;
+    $user = DB::queryFirstRow("SELECT * FROM users WHERE username=%s", $username);
+    if (!$user) {
+        $error = true;
+    } else {
+        if ($user['password'] != $pass) {
+            $error = true;
+        }
+    }
+    // decide what to render
+    if ($error) {
+        $app->render('login.html.twig', array("error" => true));
+    } else {
+        unset($user['password']);
+        $_SESSION['user'] = $user;
+        $app->render('login_success.html.twig');
+    }
+});
+
 
 $app->run();
 
