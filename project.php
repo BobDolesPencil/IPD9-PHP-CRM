4dfb71c19e84c3feb3f5b7eb9c4ee97c97bb4531
DESKTOP-NC4UFAF\vartie <vartie@gmail.com>
2017-05-17 03:28:46 -0400

    *complete the add employee template with all validations

--- project.php
+++ project.php
@@ -33,7 +33,7 @@ $twig->addGlobal('user', $_SESSION['user']);
 
 //State 1;First show
 $app->get('/', function() use($app) {
-    $app->render('login.html.twig');
+    $app->render('addemployee.html.twig');
 });
 
 $app->post('/', function() use ($app) {
@@ -57,6 +57,51 @@ $app->post('/', function() use ($app) {
         $_SESSION['user'] = $user;
         $app->render('login_success.html.twig');
     }
+});
+
+$app->post('/addemployee', function() use($app){
+    $fname = $app->request()->post('fname');
+    $lname = $app->request()->post('lname');
+    $birthdate = $app->request()->post('birthdate');
+    $hiredate = $app->request()->post('hiredate');
+    $address = $app->request()->post('address');
+    $appNo = $app->request()->post('appNo');
+    $postalcode = $app->request()->post('postalcode');
+    $country = $app->request()->post('country');
+    $email = $app->request()->post('email');
+    $phone = $app->request()->post('phone');
+    $title = $app->request()->post('title');
+    $username = $app->request()->post('username');
+    $pass = $app->request()->post('password');
+    $image = isset($_FILES['image']) ? $_FILES['image'] : array();
+    //TODO:$valuelist
+    $errorList = array();
+     if ($image) {
+        $imageInfo = getimagesize($image["tmp_name"]);
+        if (!$imageInfo) {
+            array_push($errorList, "File does not look like an valid image");
+        } else {
+            $width = $imageInfo[0];
+            $height = $imageInfo[1];
+            if ($width > 300 || $height > 300) {
+                array_push($errorList, "Image must at most 300 by 300 pixels");
+            }
+        }
+    }
+    
+    // receive data and insert
+    if (!$errorList) {
+        $imageBinaryData = file_get_contents($image['tmp_name']);
+        $mimeType = mime_content_type($image['tmp_name']);
+        //TODO:DB::insert
+        
+    } else {
+        // TODO: keep values entered on failed submission
+        $app->render('addemployee.html.twig', array(
+            'v' => $valueList
+        ));
+    }
+
 });
 
 
---
+++ scripts/addEmployee.js
@@ -0,0 +1,63 @@
+function validateForm() {
+    var x = document.forms["form"]["fname"].value;
+    if ((x.length < 2) || (x.length > 50)) {
+        window.alert("First name must be 2 to 50 characters!");
+        document.getElementById("fname").focus();
+        return false;
+    }
+    x = document.forms["form"]["lname"].value;
+    if ((x.length < 2) || (x.length > 50)) {
+        window.alert("Last name must be 2 to 50 characters!");
+        document.getElementById("lname").focus();
+        return false;
+    }
+    x = new Date(document.getElementById("birthdate").value);
+    var d = new Date();
+    if (x.getFullYear() >= (d.getFullYear()-25)) {
+        window.alert("Hire a person younger than 25 years old!!!! It is not possible.");
+        document.getElementById("birthdate").focus();
+        return false;
+    }
+    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    x = document.forms["form"]["phone"].value;
+    if (!x.match(phoneno)) {
+        window.alert("Please Enter a valid phone number!");
+        document.getElementById("phone").focus();
+        return false;
+    }
+    x = new Date(document.getElementById("hiredate").value);
+    var d = new Date();
+    if (x > d) {
+        window.alert("Enter a valid Hire Date!");
+        document.getElementById("hiredate").focus();
+        return false;
+    }
+    x = document.getElementById("username").value;
+    var username = /^([a-zA-Z0-9]{6,15})$/;
+    if (!x.match(username)) {
+        window.alert("Username must between 6-15 charachters. Mixed up alphabet and numbers only!");
+        document.getElementById("username").focus();
+        return false;
+    }
+    x = document.getElementById("postalcode").value;
+    var postalcode = /^[abceghjklmnprstvxyABCEGHJKLMNPRSTVXY][0-9][abceghjklmnprstvwxyzABCEGHJKLMNPRSTVWXYZ] ?[0-9][abceghjklmnprstvwxyzABCEGHJKLMNPRSTVWXYZ][0-9]$/;
+    if (!x.match(postalcode)) {
+        window.alert("Please Enter a valid Postal Code!");
+        document.getElementById("postalcode").focus();
+        return false;
+    }
+    var pass1 = document.getElementById("password").value;
+    var password = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if (!pass1.match(password)) {
+        window.alert("Valid Password has at least one number and one special character!");
+        document.getElementById("password").focus();
+        return false;
+    }
+    var pass2 = document.getElementById("password2").value;
+    if (pass2 != pass1) {
+        window.alert("Both Passwords must be match!");
+        document.getElementById("password2").focus();
+        return false;
+    }
+    return true;
+}
